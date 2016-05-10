e0bdcee043c0c1748d4a730324609de8a4e2e6ee
Sean Dillingham <sean.dillingham@gmail.com>
2016-05-10 11:40:51 -0700

    remove console logging from fitText.js

--- src/js/fitText.js
+++ src/js/fitText.js
@@ -3,15 +3,12 @@ window.onload = function() {
 
   // Collect large headlines that might need resizing.
   var headlines = document.getElementsByClassName("media__headline--h1");
-  console.log(headlines);
-
 
   function checkLengthAndResize(headline, index) {
 
     // Look inside the headline's hyperlink to get the text
     // character count.
     var charCount = headline.getElementsByTagName("a")[0].textContent.length;
-    console.log(charCount);
 
     // If this is a headline likely to wrap to three lines,
     // let's add a class to typeset it smaller.
