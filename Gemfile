cf875abc3c97b772e202c7149f8c7e79ab65bbfd
Sean Dillingham <sean.dillingham@gmail.com>
2016-08-10 20:55:51 -0700

    switch docs site from Jekyll to Middleman

--- Gemfile
+++ Gemfile
@@ -1,8 +1,15 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
 source 'https://rubygems.org'
 
-gem 'jekyll'
-gem 'html-proofer'
-gem 'about_yml'
-gem 'kramdown'
-gem 'rouge'
-gem 'sass'
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+gem 'middleman-syntax'
+gem 'middleman-blog'
---
+++ config.rb
@@ -0,0 +1,61 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+config[:source]       = 'documentation'
+config[:build_dir]    = '_site'
+config[:layouts_dir]  = '_layouts'
+config[:css_dir]      = 'sass'
+config[:images_dir]   = 'img'
+config[:js_dir]       = 'js'
+
+activate :directory_indexes
+activate :syntax
+activate :blog do |blog|
+  blog.prefix = "web/patterns"
+  blog.sources = "{prefix}-{title}"
+  blog.permalink = "{prefix}/{title}"
+  blog.default_extension = ".html"
+end
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  activate :syntax
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
